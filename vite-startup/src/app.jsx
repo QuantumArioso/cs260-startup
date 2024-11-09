b398b4c56b791fa22e4742c582b35af1c0e04de9
QuantumArioso <anwyntanner@gmail.com>
2024-11-09 11:44:33 -0700

    continue following tutorial for router

--- vite-startup/src/app.jsx
+++ vite-startup/src/app.jsx
@@ -1,50 +1,69 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Meetings } from './meetings/meetings';
+import { Resources } from './resources/resources';
 
 export default function App() {
   return (
-    <div className='body bg-dark text-light'>
-      <header className='container-fluid'>
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
         <nav className='navbar fixed-top navbar-dark'>
           <div className='navbar-brand'>
             Simon<sup>&reg;</sup>
           </div>
           <menu className='navbar-nav'>
             <li className='nav-item'>
-              <a className='nav-link' href='index.html'>
+              <NavLink className='nav-link' to=''>
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='play.html'>
-                Play
-              </a>
+              <NavLink className='nav-link' to='login'>
+                Login
+              </NavLink>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='scores.html'>
-                Scores
-              </a>
+              <NavLink className='nav-link' to='meetings'>
+                Meetings
+              </NavLink>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='about.html'>
-                About
-              </a>
+              <NavLink className='nav-link' to='resources'>
+                Resources
+              </NavLink>
             </li>
           </menu>
         </nav>
-      </header>
+        </header>
 
-      <main>App components go here</main>
+        <main>App components go here</main>
 
-      <footer className='bg-dark text-white-50'>
-        <div className='container-fluid'>
-          <span className='text-reset'>Author Name(s)</span>
-          <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
-            Source
-          </a>
-        </div>
-      </footer>
-    </div>
+        <Routes>
+          <Route path='/' element={<Login />} exact />
+          <Route path='/play' element={<Play />} />
+          <Route path='/scores' element={<Scores />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+
+        <footer className='bg-dark text-white-50'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Author Name(s)</span>
+            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
