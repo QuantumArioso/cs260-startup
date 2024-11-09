392c1c2481a77e611f71d1227540285f7270d2e1
QuantumArioso <anwyntanner@gmail.com>
2024-11-08 22:46:36 -0700

    starting the other .jsx files

---
+++ vite-startup/src/meetings/meetings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Meetings() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>meetings displayed here</div>
+    </main>
+  );
+}
---
+++ vite-startup/src/resources/resources.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Resources() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>resources displayed here</div>
+    </main>
+  );
+}
