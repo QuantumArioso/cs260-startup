7a11ce58da5d87db8aa757aeb8574cb6dda69e54
QuantumArioso <anwyntanner@gmail.com>
2024-11-13 20:58:36 -0700

    setup backend from simon tutorial

--- service/index.js
+++ service/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const uuid = require('uuid');
+
+app.use(express.json()); // all the data is stored in JSON
+
+// TODO: replace with my variables
+let users = {};
+let scores = [];
+
+// set up router path for the endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-app.get('*', (_req, res) => {
-  res.send({ msg: 'Startup service' });
-});
+// app.get('*', (_req, res) => {
+//   res.send({ msg: 'Startup service' });
+// });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
