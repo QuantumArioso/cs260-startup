0e113116a42e5fede45e5518a6f2f006dc680ddb
QuantumArioso <anwyntanner@gmail.com>
2024-11-26 14:22:25 -0700

    copy over simon login stuff

--- vite-startup/service/index.js
+++ vite-startup/service/index.js
@@ -1,27 +1,90 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const uuid = require('uuid');
+const DB = require('./database.js');
+
+const authCookieName = 'token';
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.use(express.json()); // all the data is stored in JSON
 
-// TODO: replace with my variables
-let user;
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the applications static content
+app.use(express.static('public'));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 
 // set up router path for the endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+// TODO: replace with my variables
+let user;
 
-// Serve up the front-end static content hosting
-app.use(express.static('public'));
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
 
-// looking at get scores in Simon and trying to figure out how it works but I have no idea how it's supposed to get a score
+    // Set the cookie
+    setAuthCookie(res, user.token);
 
-// app.get('*', (_req, res) => {
-//   res.send({ msg: 'Startup service' });
-// });
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// secureApiRouter verifies credentials for endpoints
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 var attendance = {attending: "Attendance confirmed!"};
 apiRouter.get('/attending', (_req, res) => {
--- vite-startup/src/login/login.jsx
+++ vite-startup/src/login/login.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import '../app.css';
-import { useNavigate } from 'react-router-dom';
 
-export function Login() {
-    const navigate = useNavigate();
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
 
-    const handleSubmit = (event) => {
-        event.preventDefault(); // Prevent the default form submission
-        // Perform any login logic here (e.g., authentication)
-        navigate('/meetings'); // Navigate to the meetings route
-    };
-
+export function Login({ userName, authState, onAuthChange }) {
   return (
-    <main>
-        <section>
-            <h2>Login</h2>
-            <form onSubmit={ handleSubmit }>
-                <input type="text" placeholder="Username" required></input>
-                <input type="password" placeholder="Password" required></input>
-                <button type="submit">Login</button>
-            </form>
-            <a href="#" className="create-account">Create a new account</a>
-        </section>
+    <main className='container-fluid bg-secondary text-center'>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Welcome to Simon</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
     </main>
   );
 }
